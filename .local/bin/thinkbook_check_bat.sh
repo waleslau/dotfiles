932ca2827b63697e78383eb9e8b8a362457aebfc
waleslau <waleslau@foxmail.com>
2023-04-12 10:45:49 +0800

    update

--- .local/bin/thinkbook_check_bat.sh
+++ .local/bin/thinkbook_check_bat.sh
@@ -8,4 +8,4 @@ cat /sys/class/power_supply/BAT1/uevent | grep 'CAPACITY=\<[0-7][0-9]\>' &>/dev/
 
 [ $bat_charging -eq 1 ] && echo -e '\n 正在充电～'
 
-[ $bat_full -eq 1 ] && echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode && echo '\n 已修改充电状态 \n 请重新插入电源线！'
+[ $bat_full -eq 1 ] && echo 1 | sudo tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode && echo -e '\n 已修改充电状态 \n 请重新插入电源线！'
